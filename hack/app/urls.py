b561c33791371df0b9acda6c0cca2caef4ea966c
DrkSephy <dleonar00@citymail.cuny.edu>
2015-11-14 18:46:45 -0500

    Pushing Django app

---
+++ hack/app/urls.py
@@ -0,0 +1,10 @@
+# app/urls.py
+
+from django.conf.urls import url
+
+from app import views
+
+urlpatterns = [
+  url(r'^$', views.index, name='index'),
+  url(r'^testing/$', views.test, name='test'),
+]
