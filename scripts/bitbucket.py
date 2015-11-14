fb45fc6d9cd1249333e69ad022b550f928170f40
DrkSephy <dleonar00@citymail.cuny.edu>
2015-11-14 17:18:20 -0500

    Refactoring code for Bitbucket integration

--- scripts/bitbucket.py
+++ scripts/bitbucket.py
@@ -2,17 +2,17 @@ import requests
 import moment
 import json
 
-req = requests.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/test/issues')
-data = json.loads(req.text)
+def batchBitbucketTasks(username, repository):
+	issues = requests.get('https://bitbucket.org/api/1.0/repositories' + '/' + username + '/' + repository + '/issues')
+	data = json.loads(issues.text)
+	users = []
 
-users = []
-
-for datum in data['issues']:
-	user = {}
-	if datum['responsible']['username'] == 'DrkSephy':
-		user['username'] = datum['responsible']['username']
-		user['title'] = datum['title']
-		user['priority'] = datum['metadata']['kind']
-		users.append(user)
-
+	for datum in data['issues']:
+		user = {}
+		if datum['responsible']['username'] == 'DrkSephy':
+			user['username'] = datum['responsible']['username']
+			user['title'] = datum['title']
+			user['priority'] = datum['metadata']['kind']
+			users.append(user)	
+	return users
 
