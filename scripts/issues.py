dee0be2bd62e9809d1f04aac89cb94b354044e0c
DrkSephy <dleonar00@citymail.cuny.edu>
2015-11-14 15:42:34 -0500

    Refactored issues script into functions

--- scripts/issues.py
+++ scripts/issues.py
@@ -2,21 +2,32 @@
 import requests
 import json
 
-issues = requests.get('https://api.github.com/repos/legionJS/legionJS/issues?state=all')
-data = json.loads(issues.text)
-
 # Iterate over each dictionary
 # data is an array of python dictionaries
 
-# Store all the User data (will be posted to Todoist)
-users = []
+def batchTasks(username, repository):
+	"""
+	Fetches all Github issues for the given user in a repository.
+	"""
 
-for datum in data:
-		user = {}
-		if datum['assignee'] != None:
-				user['username'] = datum['assignee']['login']
-				user['description'] = datum['body']
-				user['due'] = datum['milestone']['due_on']
-				users.append(user)
+	issues = requests.get('https://api.github.com/repos/' + username + '/' + repository + '/issues?state=all&client_id=f4c46f537e5abec0d5b0&client_secret=53ba628c38e4f8adca7d467573a13989b4546743')
+	print 'https://api.github.com/repos/' + username + '/' + repository + '/issues?state=all&client_id=f4c46f537e5abec0d5b0&client_secret=53ba628c38e4f8adca7d467573a13989b4546743'
+	data = json.loads(issues.text)
 
-print users
+	# Store all the User data (will be posted to Todoist)
+	users = []
+
+	for datum in data:
+			user = {}
+			if datum['assignee'] != None:
+					if datum['assignee']['login'] == 'DrkSephy':
+						user['username'] = datum['assignee']['login']
+						user['description'] = datum['body']
+						user['due'] = datum['milestone']['due_on']
+						users.append(user)
+	return users
+
+
+stuff = batchTasks('legionJS', 'legionJS')
+print stuff
+
