9f437da987f8abf6f069b2552ca2e254e98a4c26
DrkSephy <dleonar00@citymail.cuny.edu>
2015-11-14 16:10:58 -0500

    Implemented method for adding tasks based on Github data

--- scripts/addTask.py
+++ scripts/addTask.py
@@ -1,8 +1,31 @@
-from pytodoist import todoist
 import secrets
+import issues
+from pytodoist import todoist
 
-user = todoist.login(secrets.credentials['username'], secrets.credentials['password'])
-project = user.get_project('Github')
-project.add_task('New Task!!!!!!!!')
-user.add_project('New list')
+def sync(name):
+	"""
+	Creates a new project on todoist, and adds all Github
+	issues into the todoist project.
+	"""
+	user = todoist.login(secrets.credentials['username'], secrets.credentials['password'])
+	# name will be passed from a POST request
+	# Create a project with name passed in
+	user.add_project(name)
+	# Get a hold of the project just created
+	project = user.get_project(name)
+	# Get issue data
+	data = issues.batchTasks('legionJS', 'legionJS')
+	for task in data:
+		project.add_task(task['title'], date=task['due'])
+	return
+
+sync('Newest List')
+
+
+
+
+
+
+
+
 
