c630637d4f75cda537233eb06d37242b58790619
DrkSephy <dleonar00@citymail.cuny.edu>
2015-11-14 12:57:40 -0500

    Updating script to parse assignee names for Github issues

--- scripts/issues.py
+++ scripts/issues.py
@@ -5,4 +5,8 @@ import json
 issues = requests.get('https://api.github.com/repos/legionJS/legionJS/issues?state=all')
 data = json.loads(issues.text)
 
-
+# Iterate over each dictionary
+# data is an array of python dictionaries
+for datum in data:
+	if datum['assignee'] != None:
+		print datum['assignee']['login']
