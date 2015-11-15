3e5ab7215f1f55b98601b2f53efdced75520d26d
DrkSephy <dleonar00@citymail.cuny.edu>
2015-11-15 05:20:12 -0500

    Implementing url for database dump and updating page.html

--- hack/app/urls.py
+++ hack/app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
   url(r'^$', views.index, name='index'),
   url(r'^test/$', views.test, name='test'),
   url(r'^events/$', views.events, name='events'),
+  url(r'^fetchDatabase/$', views.fetchDatabase, name='fetchDatabase'),
 ]
