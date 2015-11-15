075deebafd5802de01c912c3edf3f0d296edf38c
DrkSephy <dleonar00@citymail.cuny.edu>
2015-11-14 20:44:53 -0500

    Adding checks for non-existent milestones

--- hack/app/scripts/issues.py
+++ hack/app/scripts/issues.py
@@ -27,6 +27,7 @@ def batchTasks(username, repository, service):
 	if service == 'github':
 		issues = requests.get('https://api.github.com/repos/' + username + '/' + repository + '/issues?state=all&client_id=f4c46f537e5abec0d5b0&client_secret=53ba628c38e4f8adca7d467573a13989b4546743')
 		data = json.loads(issues.text)
+		# print 'https://api.github.com/repos/' + username + '/' + repository + '/issues?state=all&client_id=f4c46f537e5abec0d5b0&client_secret=53ba628c38e4f8adca7d467573a13989b4546743'
 
 		# Store all the User data (will be posted to Todoist)
 		users = []
@@ -34,11 +35,13 @@ def batchTasks(username, repository, service):
 		for datum in data:
 				user = {}
 				if datum['assignee'] != None:
-						if datum['assignee']['login'] == 'DrkSephy':
+						if datum['assignee']['login'] == username:
 							user['username'] = datum['assignee']['login']
 							user['title'] = datum['title']
-							m = moment.date(datum['milestone']['due_on'], '%Y-%m-%dT%H:%M:%SZ')
-							user['due'] = m.format('YYYY-M-D H:M')
+							if datum['milestone'] != None:
+								print datum['milestone']
+								m = moment.date(datum['milestone']['due_on'], '%Y-%m-%dT%H:%M:%SZ')
+								user['due'] = m.format('YYYY-M-D H:M')
 							users.append(user)
 		return users
 	else:
