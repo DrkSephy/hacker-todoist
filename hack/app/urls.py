e4d1b3841940c978a987cafb11df960db39373cd
DrkSephy <dleonar00@citymail.cuny.edu>
2015-11-15 01:39:44 -0500

    Properly sending json back from ajax call to the client-side

--- hack/app/urls.py
+++ hack/app/urls.py
@@ -7,4 +7,5 @@ from app import views
 urlpatterns = [
   url(r'^$', views.index, name='index'),
   url(r'^test/$', views.test, name='test'),
+  url(r'^events/$', views.events, name='events'),
 ]
--- hack/app/views.py
+++ hack/app/views.py
@@ -1,20 +1,30 @@
 from scripts import issues
 from django.shortcuts import render, HttpResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 def index(request):
 	return HttpResponse('Hello!')
 
+@csrf_exempt
 def test(request):
 	if request.method == 'POST':
-		username = request.POST.get('user')
+		username = request.POST.get('username')
 		repo = request.POST.get('repo')
 		service = request.POST.get('service')
 		data = issues.batchTasks(username, repo, service)
-		return render(request, 'app/page.html', {'data': data})
+		return HttpResponse(json.dumps(data));
+		#return render(request, 'app/page.html', {'data': json.dumps(data)})
 	else:
 		print 'Not a post request?'
 		return render(request, 'app/page.html')
 
-
+@csrf_exempt
+def events(request):
+	if request.method == 'POST':
+		data = json.loads(request.body)
+		dataToRemove = data['issue']['title']
+		return render
+	return HttpResponse('Demo events')
